aa4b128ca69dc28c8af6e482855d87122fe8e761
Dark.W <pustinia@users.noreply.github.com>
2025-04-12 14:22:03 +0900

    commit

---
+++ frontend/src/layouts/components/DrawerContent.vue
@@ -0,0 +1,32 @@
+<script setup>
+import {
+  VerticalNavLink,
+  VerticalNavSectionTitle,
+} from '@layouts'
+</script>
+
+<template>
+    <ul>
+        <VerticalNavSectionTitle :item="{ heading: 'Orderservice' }" />
+        <VerticalNavLink
+            :item="{
+                title: 'Order',
+                to: '/orders',
+            }"
+        />
+        <VerticalNavSectionTitle :item="{ heading: 'Deliveryservice' }" />
+        <VerticalNavLink
+            :item="{
+                title: 'Delivery',
+                to: '/deliveries',
+            }"
+        />
+        <VerticalNavSectionTitle :item="{ heading: 'Inventoryservice' }" />
+        <VerticalNavLink
+            :item="{
+                title: 'Inventory',
+                to: '/inventories',
+            }"
+        />
+    </ul>
+</template>
